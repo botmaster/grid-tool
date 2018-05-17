772ccaec0bd82d52ee214a5d2c2921351381ff96
Pascal Achard <pascal@lahautesociete.com>
2018-05-17 18:26:47 +0200

    Add lhs logo #6135

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,113 +1,114 @@
 <template>
     <header
         class="header">
-        <div class="container">
-            <div class="header__container">
-                <div class="header__item">
-                    <label
-                        for="gridType"
-                        class="label">Type de grille</label>
-                    <div class="control">
-                        <select
-                            id="gridType"
-                            class="select"
-                            @change="changeIframeSrc">
-                            <option
-                                v-for="option in iframeList"
-                                :key="option.value"
-                                :value="option.value">
-                                {{ option.text }}
-                            </option>
-                        </select>
-                    </div>
+        <div class="header__logo">
+            <img
+                src="@/assets/lhs-logo.svg"
+                alt="LHS"
+                width="60px">
+        </div>
+        <div class="header__container">
+            <div class="header__item">
+                <label
+                    for="gridType"
+                    class="label">Type de grille</label>
+                <div class="control">
+                    <select
+                        id="gridType"
+                        class="select"
+                        @change="changeIframeSrc">
+                        <option
+                            v-for="option in iframeList"
+                            :key="option.value"
+                            :value="option.value">
+                            {{ option.text }}
+                        </option>
+                    </select>
                 </div>
-                <div class="header__item">
-                    <label
-                        for="maxWidth"
-                        class="label">Largeur maximum</label>
-                    <div class="control">
-                        <input
-                            id="maxWidth"
-                            v-model="maxWidth"
-                            class="input"
-                            type="number"
-                            min="320"
-                            max="3000"
-                            step="10">
-                        <span>px</span>
-                    </div>
-                </div>
-                <div class="header__item">
-                    <label
-                        for="colNumber"
-                        class="label">Nombre de colonnes</label>
-                    <div class="control">
-                        <input
-                            v-model.number="colCount"
-                            class="input"
-                            id="colNumber"
-                            type="number"
-                            min="1"
-                            max="30">
-                    </div>
-                </div>
-                <div class="header__item">
-                    <label
-                        for="gutterWidth"
-                        class="label">Goutières</label>
-                    <div class="control">
-                        <input
-                            v-model.number="gutterWidth"
-                            id="gutterWidth"
-                            class="input"
-                            type="number"
-                            min="0"
-                            max="400">
-                        <span>px</span>
-                    </div>
-                </div>
-                <div class="header__item">
-                    <label
-                        for="margesWidth"
-                        class="label">Marges</label>
-                    <div class="control">
-                        <input
-                            v-model.number="marginWidth"
-                            id="margesWidth"
-                            class="input"
-                            type="number"
-                            min="0"
-                            max="800">
-                        <span>px</span>
-                    </div>
-                </div>
-                <div class="header__item header__item--inline">
-                    <label
-                        for="isFluid"
-                        class="label">Grille fluide</label>
-                    <div class="control">
-                        <input
-                            v-model="isFluid"
-                            id="isFluid"
-                            type="checkbox"
-                            class="checkbox">
-                    </div>
-                    <br>
-                    <label
-                        for="isGutterFluid"
-                        class="label">Goutières Fluides</label>
-                    <div class="control">
-                        <input
-                            v-model="isGutterFluid"
-                            id="isGutterFluid"
-                            type="checkbox"
-                            class="checkbox">
-                    </div>
-                </div>
-
-
             </div>
-
+            <div class="header__item">
+                <label
+                    for="maxWidth"
+                    class="label">Largeur maximum</label>
+                <div class="control">
+                    <input
+                        id="maxWidth"
+                        v-model="maxWidth"
+                        class="input"
+                        type="number"
+                        min="320"
+                        max="3000"
+                        step="10">
+                    <span>px</span>
+                </div>
+            </div>
+            <div class="header__item">
+                <label
+                    for="colNumber"
+                    class="label">Nombre de colonnes</label>
+                <div class="control">
+                    <input
+                        v-model.number="colCount"
+                        class="input"
+                        id="colNumber"
+                        type="number"
+                        min="1"
+                        max="30">
+                </div>
+            </div>
+            <div class="header__item">
+                <label
+                    for="gutterWidth"
+                    class="label">Goutières</label>
+                <div class="control">
+                    <input
+                        v-model.number="gutterWidth"
+                        id="gutterWidth"
+                        class="input"
+                        type="number"
+                        min="0"
+                        max="400">
+                    <span>px</span>
+                </div>
+            </div>
+            <div class="header__item">
+                <label
+                    for="margesWidth"
+                    class="label">Marges</label>
+                <div class="control">
+                    <input
+                        v-model.number="marginWidth"
+                        id="margesWidth"
+                        class="input"
+                        type="number"
+                        min="0"
+                        max="800">
+                    <span>px</span>
+                </div>
+            </div>
+            <div class="header__item header__item--inline">
+                <label
+                    for="isFluid"
+                    class="label">Grille fluide</label>
+                <div class="control">
+                    <input
+                        v-model="isFluid"
+                        id="isFluid"
+                        type="checkbox"
+                        class="checkbox">
+                </div>
+                <br>
+                <label
+                    for="isGutterFluid"
+                    class="label">Goutières Fluides</label>
+                <div class="control">
+                    <input
+                        v-model="isGutterFluid"
+                        id="isGutterFluid"
+                        type="checkbox"
+                        class="checkbox">
+                </div>
+            </div>
         </div>
     </header>
 </template>
@@ -117,8 +118,7 @@ export default {
     name: 'HeaderComponent',
 
     data() {
-        return {
-        }
+        return {}
     },
     methods: {
         changeIframeSrc(e) {
@@ -131,50 +131,50 @@ export default {
 
     computed: {
         colCount: {
-            get () {
+            get() {
                 return this.$store.getters.colCount;
             },
-            set (value) {
+            set(value) {
                 this.$store.dispatch('setColCount', value);
             }
         },
         gutterWidth: {
-            get () {
+            get() {
                 return this.$store.getters.gutterWidth;
             },
-            set (value) {
+            set(value) {
                 this.$store.dispatch('setGutterWidth', value);
             }
         },
         marginWidth: {
-            get () {
+            get() {
                 return this.$store.getters.marginWidth;
             },
-            set (value) {
+            set(value) {
                 this.$store.dispatch('setMarginWidth', value);
             }
         },
         isFluid: {
-            get () {
+            get() {
                 return this.$store.getters.isFluid;
             },
-            set (value) {
+            set(value) {
                 this.$store.dispatch('setIsFluid', value);
             }
         },
         isGutterFluid: {
-            get () {
+            get() {
                 return this.$store.getters.gutterIsFluid;
             },
-            set (value) {
+            set(value) {
                 this.$store.dispatch('setGutterFluid', value);
             }
         },
         maxWidth: {
-            get () {
+            get() {
                 return this.$store.getters.maxWidth;
             },
-            set (value) {
+            set(value) {
                 this.$store.dispatch('setMaxWidth', value);
             }
         },
@@ -187,7 +187,7 @@ export default {
 
 
     },
-    created () {
+    created() {
         //this.$store.commit('setColCount', 12)
     }
 }
@@ -197,10 +197,19 @@ export default {
     .header {
         background-color: black;
         color: white;
+        display: flex;
+        align-items: center;
+
+        &__logo {
+            //background-color: red;
+            padding: 0 20px;
+            flex: 0 0 auto;
+        }
 
         &__container {
-            max-width: 1140px;
-            margin: 0 auto;
+            flex: 1;
+            /*max-width: 1140px;
+            margin: 0 auto;*/
             padding: 20px;
             display: flex;
         }
